8dfdbd3d42b05034ae41895d9c27c9d56748bddd
Valentin L <7001360+Chapeaunyra@users.noreply.github.com>
2018-08-02 08:58:31 +0200

    Create Tableaux.cs

---
+++ Cours/Tableaux.cs
@@ -0,0 +1,19 @@
+public String Test()
+        {
+            String result = "";
+            String[][] a = new string[3][];
+            a[0] = new string[4];
+            a[1] = new string[6];
+            a[2] = new string[2];
+
+            return result = a.Length + " Length de a.length" + Environment.NewLine +
+                             a[0].Length + "Length de a[0].length" + Environment.NewLine +
+                             a[1].Length + "Length de a[1].length" + Environment.NewLine +
+                             a[2].Length + "Length de a[2].length" + Environment.NewLine;
+        }
+        
+// return :
+// 3 Length de a.length
+// 4 Length de a[0].length
+// 6 Length de a[1].length
+// 2 Length de a[2].length
