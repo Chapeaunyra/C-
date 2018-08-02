4618eac24c7294b965e148e1d54998b5f07acaa0
Valentin L <7001360+Chapeaunyra@users.noreply.github.com>
2018-08-02 14:41:54 +0200

    Create DocxToText.cs

---
+++ Classes/DocxToText/DocxToText.cs
@@ -0,0 +1,37 @@
+using System;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace [nom namespace]
+{
+    class DocxToText
+    {
+        Word.Application app;
+        Word.Document doc;
+
+        public DocxToText()
+        {
+            this.app = new Word.Application();
+            this.doc = new Word.Document();
+        }
+
+        /**
+        public void OpenSaveQuit(object path, object txtPath)
+        {
+            this.doc = this.app.Documents.Open(path);
+            string text = this.doc.Content.Text;
+            this.app.Quit();
+
+            File.WriteAllText(txtPath.ToString(), text);
+        }
+        */
+
+        public String OpenReadQuit(object path)
+        {
+            this.doc = this.app.Documents.Open(path);
+            string text = this.doc.Content.Text;
+            this.app.Quit();
+
+            return text;
+        }
+    }
+}
