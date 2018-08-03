f8c2ff9e218e828065fe50ecef198f8d1bcc839d
Valentin L <7001360+Chapeaunyra@users.noreply.github.com>
2018-08-03 09:40:52 +0200

    Create GlisséDéposé.cs

---
+++ Cours/GlisséDéposé.cs
@@ -0,0 +1,28 @@
+// Pour se faire, il faut d'abords allez dans Designer.cs, choisir l'élément où l'on doit glissé-déposé le fichier 
+// et définir sa propriété AllowDrop sur True.
+
+// Ensuite, cliquez sur l'éclair (toujours dans l'onglet propriété)
+// Double cliquez sur DragEnter, la ligne suivante permet d'autoriser tout les types de fichiers dans une textBox.
+
+private void text_Box1(object sender, DragEventArgs e)
+{
+      e.Effect = DragDropEffects.All;
+}
+        
+// Retournez dans l'onglet proprété et double cliquez sur DragDrop, et rentrer le code de l'action souhaité.
+// Ici ça sera d'afficher le contenu du fichier .docx dans la textBox.
+
+tBox_reference.Text = "";
+DocxToText myDoc = new DocxToText();
+String[] files = ((String[])e.Data.GetData(DataFormats.FileDrop, false));
+String[] ligneRef;
+String refText = "";
+foreach (var file in files)
+{
+      refText = myDoc.OpenReadQuit(file);
+}
+ligneRef = refText.Split('.');
+foreach (string ligne in ligneRef)
+{
+      tBox_reference.Text += ligne + "." + Environment.NewLine;
+}
