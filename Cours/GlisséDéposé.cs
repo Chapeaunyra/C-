19d0905991f7ee3e21586fb82295d65bf837ae5c
Valentin L <7001360+Chapeaunyra@users.noreply.github.com>
2018-08-09 08:21:42 +0200

    Update GlisséDéposé.cs

--- Cours/GlisséDéposé.cs
+++ Cours/GlisséDéposé.cs
@@ -4,7 +4,7 @@
 // Ensuite, cliquez sur l'éclair (toujours dans l'onglet propriété)
 // Double cliquez sur DragEnter, la ligne suivante permet d'autoriser tout les types de fichiers dans une textBox.
 
-private void text_Box1(object sender, DragEventArgs e)
+private void text_Box1_DragEnter(object sender, DragEventArgs e)
 {
       e.Effect = DragDropEffects.All;
 }
@@ -12,17 +12,21 @@ private void text_Box1(object sender, DragEventArgs e)
 // Retournez dans l'onglet proprété et double cliquez sur DragDrop, et rentrer le code de l'action souhaité.
 // Ici ça sera d'afficher le contenu du fichier .docx dans la textBox.
 
-tBox_reference.Text = "";
-DocxToText myDoc = new DocxToText();
-String[] files = ((String[])e.Data.GetData(DataFormats.FileDrop, false));
-String[] ligneRef;
-String refText = "";
-foreach (var file in files)
-{
-      refText = myDoc.OpenReadQuit(file);
-}
-ligneRef = refText.Split('.');
-foreach (string ligne in ligneRef)
-{
-      tBox_reference.Text += ligne + "." + Environment.NewLine;
-}
+private void text_Box1_DragDrop(object sender, DragEventArgs e)
+        {
+            text_Box1.Text = "";
+            DocxToText myDoc = new DocxToText();
+            String[] files = ((String[])e.Data.GetData(DataFormats.FileDrop, false));
+            String[] ligneRef;
+            String refText = "";
+            foreach (var file in files)
+            {
+                refText = myDoc.OpenReadQuit(file);
+            }
+
+            ligneRef = refText.Split('.');
+            foreach (string ligne in ligneRef)
+            {
+                text_Box1.Text += ligne + "." + Environment.NewLine;
+            }
+        }
